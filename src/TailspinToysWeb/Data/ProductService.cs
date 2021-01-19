b3fb48f8d8a361b043ecebaa9ac6c73ceae9cc4c
thurbo-se <77664274+thurbo-se@users.noreply.github.com>
2021-01-19 18:50:18 +0100

    Update ProductService.cs

--- src/TailspinToysWeb/Data/ProductService.cs
+++ src/TailspinToysWeb/Data/ProductService.cs
@@ -13,7 +13,8 @@ namespace TailspinToysWeb.Data
                 new Product(1, "Outtop Foam Throwing Glider", "Foam throwing glider airplane", 10),
                 new Product(2, "Fascinations Metal Earth 3D Laser Cut Model", "Wright Brothers airplane made of 3D laser cut metal", 20),
                 new Product(3, "Vintage Model USMC Corsair Model Airplane", "Complete kit with realistic marking scheme", 30),
-                new Product(4, "Delta Diecast Model Airplane", "Delta Single Plane Replica", 40)
+                new Product(4, "Delta Diecast Model Airplane", "Delta Single Plane Replica", 40),
+                new Product(5, "Some Model Helicopter", "Learning example", 50)
             };
 
             return Task.FromResult(products);
